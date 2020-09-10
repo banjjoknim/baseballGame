170844d77efae124f3ab22fb1213574d48038664
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-11 00:22:16 +0900

    야구게임 진행 메서드 수정

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -17,6 +17,7 @@ public class Game {
         boolean newGame = true;
         String answer = null;
 
+        baseballGame :
         while (true) {
             if (newGame) {
                 answer = number.create();
@@ -35,20 +36,21 @@ public class Game {
             if (hint.countStrike(answer, inputStr) == 3) {
                 System.out.println("축하합니다. 3개의 숫자를 모두 맞히셨습니다!");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-                String select = bufferedReader.readLine();
-                if (restriction.checkSelect(select)) {
-                    if (choice.choose(select)) {
-                        System.out.println("새 게임을 시작합니다.");
-                        newGame = true;
-                    } else {
-                        System.out.println("수고하셨습니다. 게임을 종료합니다.");
-                        break;
+                while (true) {
+                    String select = bufferedReader.readLine();
+                    if (restriction.checkSelect(select)) {
+                        if (choice.choose(select)) {
+                            System.out.println("새 게임을 시작합니다.");
+                            newGame = true;
+                            break;
+                        } else {
+                            System.out.println("수고하셨습니다. 게임을 종료합니다.");
+                            break baseballGame;
+                        }
                     }
-                }
-                if (!restriction.checkSelect(select)) {
-                    System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
-                    continue;
+                    if (!restriction.checkSelect(select)) {
+                        System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+                    }
                 }
             }
         }
